75919d0609baef4539d2c80fded7a15ef54815c4
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-09-19 16:09:28 +0500

    npm

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,10 +1,7 @@
-const { connectToDatabase, sequelize } = require("./postgresql.config");
-
 const startServer = async (app) => {
     const PORT = process.env.PORT || 3000;
 
     try {
-        const sequelize = await connectToDatabase()
         app.listen(process.env.PORT, () => {
             console.warn(`server is running on http://localhost:${PORT}/`)
         })
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 const koa = require("koa");
 const bodyParser = require("koa-bodyparser");
-const Router = require("koa-router");
 const startServer = require("./config/server.config");
 require("dotenv").config();
 const app = new koa();
-const protectedRoutes = require("./routes/protected.routes")
-const publicRoutes = require("./routes/userPublic.routes")
 
 
 app.use(bodyParser());
-const mainRouter = new Router();
-
-mainRouter.use(publicRoutes.userPublicRoutes.routes());
-mainRouter.use(publicRoutes.userPublicRoutes.allowedMethods());
-mainRouter.use(protectedRoutes.protectedRoutes.routes());
-mainRouter.use(protectedRoutes.protectedRoutes.allowedMethods());
-
-app.use(mainRouter.routes());
-app.use(mainRouter.allowedMethods());
 
 startServer(app)
